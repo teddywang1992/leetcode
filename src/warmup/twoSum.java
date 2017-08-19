01279968ddc86540420235494d8c6759cf3a1bb2
teddywang <443909723@qq.com>
2017-08-19 11:27:11 -0700

    twoSumHashMap的做法

--- src/warmup/twoSum.java
+++ src/warmup/twoSum.java
@@ -1,16 +1,18 @@
 package warmup;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class twoSum {
     public static void main(String[] args) throws IllegalAccessException {
         int[] input = {2, 7, 11, 15};
         int target = 9;
         twoSum obj = new twoSum();
-        System.out.println(Arrays.toString(obj.twoSumFunction(input, target)));
+        System.out.println(Arrays.toString(obj.twoSumFunctionHashMap(input, target)));
     }
 
-    public int[] twoSumFunction(int[] input, int target) throws IllegalAccessException {
+    public int[] twoSumFunctionSillyMethod(int[] input, int target) throws IllegalAccessException {
         for (int i = 0; i < input.length; i++) {
             for (int j = input.length - 1; j > i; j--) {
                 if (input[i] + input[j] == target)
@@ -18,5 +20,18 @@ public class twoSum {
             }
         }
         throw new IllegalArgumentException("No two sum solution");
+    }
+    public int[] twoSumFunctionHashMap(int[] input, int target) throws IllegalAccessException {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < input.length; i++) {
+            map.put(input[i], i);
+        }
+        for (int i = 0; i < input.length; i++) {
+            int com = target - input[i];
+            if(map.containsKey(com) && map.get(com) != i) {
+                return new int[] {map.get(com), i};
+            }
+        }
+        throw new IllegalArgumentException("no result");
     }
 }
