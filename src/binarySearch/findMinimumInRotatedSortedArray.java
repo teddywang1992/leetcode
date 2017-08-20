e02e5f9715f29c9f96bd4d083f21edf94b22911d
teddywang <443909723@qq.com>
2017-08-20 12:43:08 -0700

    二分法找rotated sorted array，注意设置target时候要从后面向前搜索

---
+++ src/binarySearch/findMinimumInRotatedSortedArray.java
@@ -0,0 +1,28 @@
+package binarySearch;
+
+public class findMinimumInRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] test = {1,2,3};
+        findMinimumInRotatedSortedArray obj = new findMinimumInRotatedSortedArray();
+        System.out.println(obj.findMin(test));
+
+    }
+    private int findMin(int[] nums) {
+        if (nums.length == 0 || nums == null) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int target = nums[end];
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > target) {
+                start = mid;
+            }
+            else {
+                end = mid;
+            }
+        }
+        return Math.min(nums[start], nums[end]);
+    }
+}
