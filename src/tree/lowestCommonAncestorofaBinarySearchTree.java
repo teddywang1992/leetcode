0ad0666b3ce67c10245d7faa436b9b448fc22740
teddywang <443909723@qq.com>
2017-08-22 15:42:35 -0700

    compare the number in bst.

---
+++ src/tree/lowestCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,20 @@
+package tree;
+
+public class lowestCommonAncestorofaBinarySearchTree {
+    public static void main(String[] args) {
+        lowestCommonAncestorofaBinarySearchTree obj = new lowestCommonAncestorofaBinarySearchTree();
+        TreeNode root = constantTree.returnBST();
+        TreeNode left = constantTree.getNode4();
+        TreeNode right = constantTree.getNode6();
+        System.out.println("ancestor is: " + obj.lowestCommonAncestor(root, left, right).val);
+    }
+    private TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if(root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
