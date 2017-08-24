17d9161034896eaaf6ba50be0b9c966059066501
teddywang <443909723@qq.com>
2017-08-24 15:45:34 -0700

    similar to triangle

--- src/dynamicProgramming/minimumPathSum.java
+++ src/dynamicProgramming/minimumPathSum.java
@@ -5,12 +5,10 @@ public class minimumPathSum {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return -1;
         }
-
         //state
         int n = grid.length;
         int m = grid[0].length;
         int[][] f = new int[n][m];
-
         //init
         f[0][0] = grid[0][0];
         for (int i = 1; i < n; i++) {
@@ -19,14 +17,12 @@ public class minimumPathSum {
         for (int i = 1; i < m; i++) {
             f[0][i] = f[0][i - 1] + grid[0][i];
         }
-
         //top down
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 f[i][j] = Math.min(f[i - 1][j], f[i][j - 1]) + grid[i][j];
             }
         }
-
         //answer
         return f[n - 1][m - 1];
     }
