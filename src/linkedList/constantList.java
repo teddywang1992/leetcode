1b55c3fd1b1e6df3a14f0dc7026a6d54959775a6
teddywang <443909723@qq.com>
2017-08-28 16:44:17 -0700

    make it more readable

--- src/linkedList/constantList.java
+++ src/linkedList/constantList.java
@@ -1,13 +1,13 @@
 package linkedList;
 
-public class constantList {
+class constantList {
     private static ListNode head = new ListNode(1);
     private static ListNode node2 = new ListNode(1);
     private static ListNode node3 = new ListNode(2);
     private static ListNode node4 = new ListNode(3);
-    private static ListNode node5 = new ListNode(3);
+    private static ListNode node5 = new ListNode(4);
 
-    public static ListNode returnNormalHead() {
+    static ListNode returnNormalHead() {
         head.next = node2;
         node2.next = node3;
         node3.next = node4;
