fed4ab0a333023efdf5691457c2f57c060e3faf4
teddywang <443909723@qq.com>
2017-08-29 12:15:57 -0700

    in the process of merge. Be careful about the situation when one list is null.

---
+++ src/linkedList/mergeTwoSortedLists.java
@@ -0,0 +1,45 @@
+package linkedList;
+
+public class mergeTwoSortedLists {
+    public static void main(String[] args) {
+        mergeTwoSortedLists obj = new mergeTwoSortedLists();
+        ListNode head1 = constantList.returnNormalHead();
+        ListNode head2 = constantList.returnNormalHead2();
+        ListNode result = obj.mergeTwoLists(head1, head2);
+        while(result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null && l2 == null) {
+            return null;
+        }
+        if(l1 == null) {
+            return l2;
+        }
+        if(l2 == null) {
+            return l1;
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                head.next = l1;
+                head = head.next;
+                l1 = l1.next;
+            } else {
+                head.next = l2;
+                head = head.next;
+                l2 = l2.next;
+            }
+        }
+        if(l1 != null) {
+            head.next = l1;
+        } else {
+            head.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
