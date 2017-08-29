b7c631a7acba46dbf1fda5b694a0224d143bc00b
teddywang <443909723@qq.com>
2017-08-29 12:12:54 -0700

    create a new Linked list

--- src/linkedList/constantList.java
+++ src/linkedList/constantList.java
@@ -1,18 +1,31 @@
 package linkedList;
 
 class constantList {
-    private static ListNode head = new ListNode(1);
+    private static ListNode head1 = new ListNode(1);
     private static ListNode node2 = new ListNode(1);
     private static ListNode node3 = new ListNode(2);
     private static ListNode node4 = new ListNode(3);
     private static ListNode node5 = new ListNode(4);
+    private static ListNode head2 = new ListNode(0);
+    private static ListNode node6 = new ListNode(3);
+    private static ListNode node7 = new ListNode(4);
+    private static ListNode node8 = new ListNode(7);
+    private static ListNode node9 = new ListNode(9);
 
     static ListNode returnNormalHead() {
-        head.next = node2;
+        head1.next = node2;
         node2.next = node3;
         node3.next = node4;
         node4.next = node5;
         node5.next = null;
-        return head;
+        return head1;
+    }
+    static ListNode returnNormalHead2() {
+        head2.next = node6;
+        node6.next = node7;
+        node7.next = node8;
+        node8.next = node9;
+        node9.next = null;
+        return head2;
     }
 }
