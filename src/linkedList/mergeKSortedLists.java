0ab9523acf2c1936ea7b5dfaf75c6871cb2a311e
teddywang <443909723@qq.com>
2017-08-31 20:07:58 -0700

    1, use priorityQueue to implement a heap 2, divide conquer

--- src/linkedList/mergeKSortedLists.java
+++ src/linkedList/mergeKSortedLists.java
@@ -15,19 +15,63 @@ public class mergeKSortedLists {
         ListNode head1 = constantList.returnNormalHead();
         ListNode head2 = constantList.returnNormalHead2();
         ListNode[] lists = {head1, head2};
-        ListNode result = mergeKLists(lists);
+        ListNode result = mergeKLists2(lists);
         while(result != null) {
             System.out.println(result.val);
             result = result.next;
         }
 
     }
-    private static ListNode mergeKLists(ListNode[] lists) {
+    private static ListNode mergeKLists2(ListNode[] lists) {
+        if(lists.length == 0 || lists == null) {
+            return null;
+        }
+
+        return mergeKListsHelper(lists, 0, lists.length - 1);
+    }
+
+    private static ListNode mergeKListsHelper(ListNode[] lists, int start, int end) {
+        //递归出口
+        if(start == end) {
+            return lists[start];
+        }
+
+        int mid = start + (end - start) / 2;
+        ListNode left = mergeKListsHelper(lists, start, mid);
+        ListNode right = mergeKListsHelper(lists, mid + 1, end);
+
+        return mergeTwoLists(left, right);
+    }
+
+    private static ListNode mergeTwoLists(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
+
+        while(left != null && right != null) {
+            if(left.val < right.val) {
+                node.next = left;
+                node = node.next;
+                left = left.next;
+            } else {
+                node.next = right;
+                node = node.next;
+                right = right.next;
+            }
+        }
+        if(left != null) {
+            node.next = left;
+        } else {
+            node.next = right;
+        }
+
+        return dummy.next;
+    }
+    private static ListNode mergeKLists1(ListNode[] lists) {
         if(lists == null || lists.length == 0) {
             return null;
         }
 
-        Queue<ListNode> heap = new PriorityQueue<ListNode>(lists.length, ListNodeComparator);
+        Queue<ListNode> heap = new PriorityQueue<>(lists.length, ListNodeComparator);
         for(int i = 0; i < lists.length; i++) {
             heap.add(lists[i]);
         }
