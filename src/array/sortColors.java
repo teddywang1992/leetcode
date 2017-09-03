4303d4d5954d8640fc88b1fee2f9ecd3cef69ba6
teddywang <443909723@qq.com>
2017-09-03 10:57:40 -0700

    two times of partition two arrays

---
+++ src/array/sortColors.java
@@ -0,0 +1,31 @@
+package array;
+
+public class sortColors {
+    private void sortColors(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return;
+        }
+
+        int left = 0;
+        int i = 0;
+        int right = nums.length - 1;
+
+        while(i <= right) {
+            if(nums[i] == 0) {
+                swap(nums, i, left);
+                i++;
+                left++;
+            } else if(nums[i] == 1) {
+                i++;
+            } else {
+                swap(nums, i, right);
+                right--;
+            }
+        }
+    }
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
