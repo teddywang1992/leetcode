2e465adb9f9d6ff6535e369d73ce851592a99a74
teddywang <443909723@qq.com>
2017-09-11 23:38:03 -0700

    store current sum in variable result. store 进位值到carry.

---
+++ src/string/addBinary.java
@@ -0,0 +1,27 @@
+package string;
+
+public class addBinary {
+    public static void main(String[] args) {
+        String a = "11";
+        String b = "1";
+        System.out.println(addBinary(a, b));
+    }
+    private static String addBinary(String a, String b) {
+        String result = "";
+        int carry = 0;
+
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
+            int sum = carry;
+            sum += (i >= 0) ? a.charAt(i) - '0' : 0;
+            sum += (j >= 0) ? b.charAt(j) - '0' : 0;
+            result = (sum % 2) + result;
+            carry = sum / 2;
+        }
+
+        if (carry != 0) {
+            result = carry + result;
+        }
+
+        return result;
+    }
+}
