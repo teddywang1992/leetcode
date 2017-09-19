81bd6f87b5f4eb91ace2dd36d5f81caf49154721
teddywang <443909723@qq.com>
2017-09-18 22:26:24 -0700

    dfs

---
+++ src/graph_BFS_DFS/NestedListWeightSum.java
@@ -0,0 +1,25 @@
+package graph_BFS_DFS;
+
+public class NestedListWeightSum {
+//    public int depthSum(List<NestedInteger> nestedList) {
+//        int result = 0;
+//
+//        for (NestedInteger nestedInteger : nestedList) {
+//            result += depthSumHelper(nestedInteger, 1);
+//        }
+//
+//        return result;
+//    }
+//
+//    private int depthSumHelper(NestedInteger nestedList, int depth) {
+//        int result = 0;
+//
+//        if (nestedList.isInteger()) return depth * nestedList.getInteger();
+//
+//        for (NestedInteger element : nestedList.getList()) {
+//            result += depthSumHelper(element, depth + 1);
+//        }
+//
+//        return result;
+//    }
+}
