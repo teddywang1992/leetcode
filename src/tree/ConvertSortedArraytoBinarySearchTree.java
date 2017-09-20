78e33eb67b9cf23e3c8024c6abfcd0fc9c5d4c0a
teddywang <443909723@qq.com>
2017-09-19 17:52:38 -0700

    二分法

---
+++ src/tree/ConvertSortedArraytoBinarySearchTree.java
@@ -0,0 +1,18 @@
+package tree;
+
+public class ConvertSortedArraytoBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode helper(int[] nums, int start, int end) {
+        if (start > end) return null;
+        int mid = (start + end) / 2;
+        TreeNode current = new TreeNode(nums[mid]);
+
+        current.left = helper(nums, start, mid - 1);
+        current.right = helper(nums, mid + 1, end);
+
+        return current;
+    }
+}
