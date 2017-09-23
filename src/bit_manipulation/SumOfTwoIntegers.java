6ff993b330f760fcb89481badd86fec61fac5ebf
teddywang <443909723@qq.com>
2017-09-22 20:47:42 -0700

    legacy

--- src/bit_manipulation/SumOfTwoIntegers.java
+++ src/bit_manipulation/SumOfTwoIntegers.java
@@ -1,4 +1,7 @@
 package bit_manipulation;
+
+import java.util.*;
+
 /*
 然后我们进一步分析，如果得到上面的第一第二种情况，我们在二进制下来看，不考虑进位的加，0+0=0
 0+1=1, 1+0=1， 1+1=0，这就是异或的运算规则，如果只考虑进位的加0+0=0, 0+1=0, 1+0=0, 1+1=1，而这其
@@ -6,14 +9,95 @@ package bit_manipulation;
 返回第一步的结果
  */
 public class SumOfTwoIntegers {
-    public int getSum(int a, int b) {
-        if (b == 0) {
-            return a;
+    public static Map<Integer, List<Integer>> createAdjacencyLists(List<String> employees, List<String> friendships) {
+        // IMPLEMENTATION GOES HERE
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        for (String s : employees) {
+            int cur = s.charAt(0) - '0';
+            map.put(cur, new ArrayList<>());
         }
 
-        int sum = a ^ b;
-        int carry = (a & b) << 1;
+        for (String s : friendships) {
+            int me = s.charAt(0) - '0';
+            int friend = s.charAt(2) - '0';
 
-        return getSum(sum, carry);
+            map.get(me).add(friend);
+            map.get(friend).add(me);
+        }
+
+        return map;
+    }
+
+    /* START TEST CASES
+     * You can add test cases in the three functions below: employeesInputs(),
+     * friendshipsInputs(), and expectedOutputs(). Each test case input/expected output
+     * should correspond to the same index in the respective lists returned.
+     */
+    public static List<List<String>> employeesInputs() {
+        List<List<String>> inputs = new ArrayList<>();
+
+        inputs.add(Arrays.asList(
+                "1,Richard,Engineering",
+                "2,Erlich,HR",
+                "3,Monica,Business",
+                "4,Dinesh,Engineering",
+                "6,Carla,Engineering",
+                "9,Laurie,Directors"
+        ));
+
+        return inputs;
+    }
+
+    public static List<List<String>> friendshipsInputs() {
+        List<List<String>> inputs = new ArrayList<>();
+
+        inputs.add(Arrays.asList(
+                "1,2",
+                "1,3",
+                "1,6",
+                "2,4"
+        ));
+
+        return inputs;
+    }
+
+    public static List<Map<Integer, List<Integer>>> expectedOutputs() {
+        List<Map<Integer, List<Integer>>> expected = new ArrayList<>();
+
+        Map<Integer, List<Integer>> output = new HashMap<>();
+        output.put(1, Arrays.asList(2, 3, 6));
+        output.put(2, Arrays.asList(1, 4));
+        output.put(3, Arrays.asList(1));
+        output.put(4, Arrays.asList(2));
+        output.put(6, Arrays.asList(1));
+        output.put(9, new ArrayList<Integer>());
+        expected.add(output);
+
+        return expected;
+    }
+    // END TEST CASES
+
+    // DO NOT MODIFY MAIN()
+    public static void main(String[] args) {
+        List<List<String>> employeesInputs = employeesInputs();
+        List<List<String>> friendshipsInputs = friendshipsInputs();
+        List<Map<Integer, List<Integer>>> expectedOutputs = expectedOutputs();
+
+        for (int i = 0; i < employeesInputs.size(); i++) {
+            try {
+                Map<Integer, List<Integer>> result = createAdjacencyLists(employeesInputs.get(i), friendshipsInputs.get(i));
+                Map<Integer, List<Integer>> expected = expectedOutputs.get(i);
+
+                if (result.equals(expected)) {
+                    System.out.println("Pass");
+                } else {
+                    System.out.println("Fail");
+                }
+            } catch (Exception e) {
+                System.out.println("Fail");
+                System.out.println(e);
+            }
+        }
     }
 }
--- src/dynamicProgramming/BestTimeToBuyAndSellStock4.java
+++ src/dynamicProgramming/BestTimeToBuyAndSellStock4.java
@@ -1,29 +1,27 @@
 package dynamicProgramming;
 
 public class BestTimeToBuyAndSellStock4 {
-    public class Solution {
-        public int maxProfit(int k, int[] prices) {
-            if(k >= prices.length / 2) {
-                int maxProfit = 0;
-                for(int i = 1; i < prices.length; i++) {
-                    if(prices[i] > prices[i - 1]) maxProfit += prices[i] - prices[i - 1];
-                }
-                return maxProfit;
+    public int maxProfit(int k, int[] prices) {
+        if(k >= prices.length / 2) {
+            int maxProfit = 0;
+            for(int i = 1; i < prices.length; i++) {
+                if(prices[i] > prices[i - 1]) maxProfit += prices[i] - prices[i - 1];
             }
-
-            // dp[i, j] represents the max profit up until prices[j] using at most i transactions.
-            int dp[][] = new int[k + 1][prices.length];
-
-            for(int i = 1; i <= k; i++) {
-                int localMax = dp[i - 1][0] - prices[0];
-
-                for(int j = 1; j < prices.length; j++) {
-                    dp[i][j] = Math.max(dp[i][j - 1], prices[j] + localMax);
-                    localMax = Math.max(localMax, dp[i - 1][j] - prices[j]);
-                }
-            }
-
-            return dp[k][prices.length - 1];
+            return maxProfit;
         }
+
+        // dp[i, j] represents the max profit up until prices[j] using at most i transactions.
+        int dp[][] = new int[k + 1][prices.length];
+
+        for(int i = 1; i <= k; i++) {
+            int localMax = dp[i - 1][0] - prices[0];
+
+            for(int j = 1; j < prices.length; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], prices[j] + localMax);
+                localMax = Math.max(localMax, dp[i - 1][j] - prices[j]);
+            }
+        }
+
+        return dp[k][prices.length - 1];
     }
 }
