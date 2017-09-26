e78e82cff151ed3b01533ae2b8e5f11a99707325
teddywang <443909723@qq.com>
2017-09-25 17:19:53 -0700

    json read write

--- src/warmup/PostGetJSON.java
+++ src/warmup/PostGetJSON.java
@@ -1,4 +1,37 @@
 package warmup;
 
+import org.json.*;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+
 public class PostGetJSON {
+    public static void main(String[] args) throws IOException, JSONException {
+        JSONArray json = readJsonFromUrl("http://jsonplaceholder.typicode.com/users");
+        System.out.println(json.toString());
+        System.out.println(json.);
+    }
+
+    public static JSONArray readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONArray json = new JSONArray(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
 }
