a0bfd358024f0aff14b8ff7e57b16e7f1361982f
teddywang <443909723@qq.com>
2017-09-26 17:04:55 -0700

    dp

---
+++ src/array/LongestContinuousIncreasingSubsequence.java
@@ -0,0 +1,24 @@
+package array;
+
+public class LongestContinuousIncreasingSubsequence {
+    public int findLengthOfLCIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int max = 1;
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                dp[i] = dp[i - 1] + 1;
+            } else {
+                dp[i] = 1;
+            }
+
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+}
