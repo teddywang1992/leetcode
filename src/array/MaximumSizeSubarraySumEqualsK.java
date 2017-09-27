c4db00a3fd5a1f53f4fec762d94e85117c65ad06
teddywang <443909723@qq.com>
2017-09-26 19:19:30 -0700

    facebook

---
+++ src/array/MaximumSizeSubarraySumEqualsK.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaximumSizeSubarraySumEqualsK {
+    public int maxSubArrayLen(int[] nums, int k) {
+        // if (nums == null || nums.length == 0) {
+        //     return 0;
+        // }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum == k) max = i + 1;
+            else if (map.containsKey(sum - k)) max = Math.max(max, i - map.get(sum - k));
+            if (!map.containsKey(sum)) map.put(sum, i);
+        }
+
+        return max;
+    }
+}
