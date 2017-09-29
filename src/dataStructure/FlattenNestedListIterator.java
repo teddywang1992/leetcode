615caba7c7af63f3ed5569acd2dbe7d262fa0225
teddywang <443909723@qq.com>
2017-09-28 21:26:57 -0700

    facebook

---
+++ src/dataStructure/FlattenNestedListIterator.java
@@ -0,0 +1,28 @@
+package dataStructure;
+
+public class FlattenNestedListIterator {
+//    private Stack<NestedInteger> stack = new Stack<>();
+//
+//    public NestedIterator(List<NestedInteger> nestedList) {
+//        for (int i = nestedList.size() - 1; i >=0; i--) {
+//            stack.push(nestedList.get(i));
+//        }
+//    }
+//
+//    @Override
+//    public Integer next() {
+//        return stack.pop().getInteger();
+//    }
+//
+//    @Override
+//    public boolean hasNext() {
+//        while (!stack.isEmpty() && !stack.peek().isInteger()) {
+//            List<NestedInteger> list = stack.pop().getList();
+//            for (int i = list.size() - 1; i >= 0; i--) {
+//                stack.push(list.get(i));
+//            }
+//        }
+//
+//        return !stack.isEmpty();
+//    }
+}
