e4e7fa7d49ebb6e53b1efaa2fe91521143165b3b
teddywang <443909723@qq.com>
2017-10-02 15:01:45 -0700

    ebay# Please enter the commit message for your changes. Lines starting

---
+++ src/ebay/ShuffleAnArray.java
@@ -0,0 +1,38 @@
+package ebay;
+
+import java.util.Random;
+
+public class ShuffleAnArray {
+    int[] nums;
+    Random random;
+
+    public ShuffleAnArray(int[] nums) {
+        this.nums = nums;
+        random = new Random();
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return nums;
+    }
+
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        if (nums == null) return null;
+
+        int[] shuffleNums = nums.clone();
+
+        for (int i = 0; i < nums.length; i++) {
+            int j = random.nextInt(i + 1);
+            swap(shuffleNums, i, j);
+        }
+
+        return shuffleNums;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
