0e54c74595a94d1585ca3d9b776bb793a4cac392
teddywang <443909723@qq.com>
2017-10-03 14:30:20 -0700

    ebay

---
+++ src/ebay/trimTree.java
@@ -0,0 +1,19 @@
+package ebay;
+
+//669. Trim a Binary Search Tree
+
+import tree.TreeNode;
+
+public class trimTree {
+    private static TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) return null;
+
+        if (root.val < L) return trimBST(root.right, L, R);
+        if (root.val > R) return trimBST(root.left, L, R);
+
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+
+        return root;
+    }
+}
