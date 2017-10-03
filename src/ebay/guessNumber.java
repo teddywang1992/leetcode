97c3498846c657fed1797c3d5088a80f14e81266
teddywang <443909723@qq.com>
2017-10-03 14:13:14 -0700

    ebay

---
+++ src/ebay/guessNumber.java
@@ -0,0 +1,23 @@
+package ebay;
+
+//374. Guess Number Higher or Lower
+
+public class guessNumber {
+//    public int guessNumber(int n) {
+//        if (n == 0) return 0;
+//
+//        int start = 0;
+//        int end = n;
+//
+//        while (start + 1 < end) {
+//            int mid = start + (end - start) / 2;
+//            int temp = guess(mid);
+//
+//            if (temp == 0) return mid;
+//            else if (temp == -1) end = mid;
+//            else start = mid;
+//        }
+//
+//        return guess(start) == 0 ? start : end;
+//    }
+}
