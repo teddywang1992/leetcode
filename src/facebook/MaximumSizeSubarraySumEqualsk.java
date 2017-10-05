3e30f48a6700c28f03bed2a7a5ec7ceb81653702
teddywang <443909723@qq.com>
2017-10-04 22:37:47 -0700

    facebook

---
+++ src/facebook/MaximumSizeSubarraySumEqualsk.java
@@ -0,0 +1,31 @@
+package facebook;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaximumSizeSubarraySumEqualsk {
+    public static void main(String[] args) {
+        int[] nums = {1, -1, 5, -2, 3};
+        System.out.println(maxSubArrayLen(nums, 3));
+    }
+
+    private static int maxSubArrayLen(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int max = 0;
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) max = Math.max(max, i - map.get(sum - k));
+            else if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        return max;
+    }
+}
