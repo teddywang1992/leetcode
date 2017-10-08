ed5991d835c3d1b9347539f9d6c818444c22305a
teddywang <443909723@qq.com>
2017-10-07 19:39:36 -0700

    ebay

---
+++ src/ebay/FactorialTrailingZeroes.java
@@ -0,0 +1,14 @@
+package ebay;
+
+public class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int count = 0;
+
+        while (n != 0) {
+            count += n / 5;
+            n = n / 5;
+        }
+
+        return count;
+    }
+}
