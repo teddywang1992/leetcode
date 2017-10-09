b28b3eaee95a4183fe4950f923ba758252f16117
teddywang <443909723@qq.com>
2017-10-09 12:58:01 -0700

    ebay

---
+++ src/ebay/LengthOfLastWord.java
@@ -0,0 +1,9 @@
+package ebay;
+
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int lastIndex = s.lastIndexOf(' ') + 1;
+        return s.length() - lastIndex;
+    }
+}
