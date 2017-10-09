dc4ee482e4d7da1bd4609b645646bb32f3f633b6
teddywang <443909723@qq.com>
2017-10-08 21:55:47 -0700

    ebay

---
+++ src/ebay/ReverseBits.java
@@ -0,0 +1,14 @@
+package ebay;
+
+public class ReverseBits {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n >>>= 1;
+            if (i < 31) result <<= 1;
+        }
+
+        return result;
+    }
+}
