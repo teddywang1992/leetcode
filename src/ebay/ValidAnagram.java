6edf5afd82afb09eb4d0ea4a19d145697300fd10
teddywang <443909723@qq.com>
2017-10-08 17:38:46 -0700

    ebay

---
+++ src/ebay/ValidAnagram.java
@@ -0,0 +1,24 @@
+package ebay;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+        Arrays.fill(count, 0);
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+
+        return true;
+    }
+}
