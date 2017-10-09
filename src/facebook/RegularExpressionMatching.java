60accd59216f7b0337866fa508f1c93a1a595781
teddywang <443909723@qq.com>
2017-10-09 16:07:09 -0700

    facebook

---
+++ src/facebook/RegularExpressionMatching.java
@@ -0,0 +1,27 @@
+package facebook;
+
+public class RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+
+        for (int i = 2; i <= p.length(); i++) {
+            if(p.charAt(i-1) == '*') {
+                dp[0][i] = dp[0][i - 2];
+            }
+        }
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (p.charAt(j - 1) == '.' || s.charAt(i - 1) == p.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.'));
+                }
+            }
+        }
+
+        return dp[s.length()][p.length()];
+    }
+}
