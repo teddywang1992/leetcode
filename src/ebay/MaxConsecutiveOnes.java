02aaf1aa2d3866155a83bf918925251e81aadd10
teddywang <443909723@qq.com>
2017-10-10 16:51:10 -0700

    ebay

---
+++ src/ebay/MaxConsecutiveOnes.java
@@ -0,0 +1,18 @@
+package ebay;
+
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                max = Math.max(max, count);
+            }
+
+            if (nums[i] == 0) count = 0;
+        }
+
+        return max;
+    }
+}
