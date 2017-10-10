08d6a54f1ee9b14fc178aabe1572b978a87f8acf
teddywang <443909723@qq.com>
2017-10-10 16:38:11 -0700

    ebay

---
+++ src/ebay/EmployeeImportance.java
@@ -0,0 +1,37 @@
+package ebay;
+
+public class EmployeeImportance {
+//    private int value;
+//    private Set<Employee> set;
+//    public int getImportance(List<Employee> employees, int id) {
+//        value = 0;
+//        set = new HashSet<>();
+//        Employee target = findById(employees, id);
+//        helper(employees, target);
+//
+//
+//        return value;
+//    }
+//
+//    private void helper(List<Employee> employees, Employee employee) {
+//        if (set.contains(employee)) return;
+//
+//        value += employee.importance;
+//        set.add(employee);
+//
+//        for (int subId : employee.subordinates) {
+//            Employee cur = findById(employees, subId);
+//            helper(employees, cur);
+//        }
+//    }
+//
+//    private Employee findById(List<Employee> employees, int id) {
+//        for (Employee employee : employees) {
+//            if (employee.id == id) {
+//                return employee;
+//            }
+//        }
+//
+//        return null;
+//    }
+}
