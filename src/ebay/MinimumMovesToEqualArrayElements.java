958fa2f58a81adf9c8e5d89c4be95484a7048a2c
teddywang <443909723@qq.com>
2017-10-09 19:58:12 -0700

    ebay

---
+++ src/ebay/MinimumMovesToEqualArrayElements.java
@@ -0,0 +1,16 @@
+package ebay;
+
+public class MinimumMovesToEqualArrayElements {
+    public int minMoves(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            sum += num;
+            min = Math.min(min, num);
+        }
+
+        return sum - min * nums.length;
+    }
+}
