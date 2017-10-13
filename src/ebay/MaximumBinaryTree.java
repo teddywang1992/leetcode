12e00b6af1f2d6ea74cfe0d08a749483ffbadaf4
teddywang <443909723@qq.com>
2017-10-12 21:30:20 -0700

    ebay

---
+++ src/ebay/MaximumBinaryTree.java
@@ -0,0 +1,28 @@
+package ebay;
+
+import tree.TreeNode;
+
+public class MaximumBinaryTree {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if (nums == null || nums.length == 0) return null;
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode helper(int[] nums, int start, int end) {
+        if (start > end) return null;
+
+        int maxIndex = start;
+        for (int i = start; i <= end; i++) {
+            if (nums[maxIndex] < nums[i]) {
+                maxIndex = i;
+            }
+        }
+
+        TreeNode root = new TreeNode(nums[maxIndex]);
+
+        root.left = helper(nums, start, maxIndex - 1);
+        root.right = helper(nums, maxIndex + 1, end);
+
+        return root;
+    }
+}
