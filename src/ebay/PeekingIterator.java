ee5eb12051d34d905612faa1799b42ae64ec14dc
teddywang <443909723@qq.com>
2017-10-13 14:50:18 -0700

    ebay

---
+++ src/ebay/PeekingIterator.java
@@ -0,0 +1,30 @@
+package ebay;
+
+import java.util.Iterator;
+
+public class PeekingIterator {
+    private Iterator<Integer> iterator;
+    private Integer next;
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        this.iterator = iterator;
+        next = iterator.hasNext() ? iterator.next() : null;
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return next;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    public Integer next() {
+        int result = next;
+        next = iterator.hasNext() ? iterator.next() : null;
+        return result;
+    }
+
+    public boolean hasNext() {
+        return next != null;
+    }
+}
