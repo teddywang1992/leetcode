c856f71eeb02a13aebd9297df20c37512f101e1b
teddywang <443909723@qq.com>
2017-10-12 20:58:42 -0700

    ebay

---
+++ src/ebay/UniqueBinarySearchTrees.java
@@ -0,0 +1,18 @@
+package ebay;
+
+public class UniqueBinarySearchTrees {
+    public int numTrees(int n) {
+        if (n <= 1) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[i - j - 1] * dp[j];
+            }
+        }
+
+        return dp[n];
+    }
+}
