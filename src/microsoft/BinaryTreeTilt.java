bfdf69d21376c782fb8091c1dc299973d906cb86
teddywang <443909723@qq.com>
2017-10-17 14:22:16 -0700

    microsoft

---
+++ src/microsoft/BinaryTreeTilt.java
@@ -0,0 +1,23 @@
+package microsoft;
+
+import tree.TreeNode;
+
+public class BinaryTreeTilt {
+    int result = 0;
+
+    public int findTilt(TreeNode root) {
+        postOrder(root);
+        return result;
+    }
+
+    private int postOrder(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = postOrder(root.left);
+        int right = postOrder(root.right);
+
+        result += Math.abs(left - right);
+
+        return left + right + root.val;
+    }
+}
