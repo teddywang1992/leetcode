59d41a42d991b671c5c4ec856bfbc7e9a775e1c3
teddywang <443909723@qq.com>
2017-10-21 20:41:33 -0700

    microsoft

---
+++ src/microsoft/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,15 @@
+package microsoft;
+
+import tree.TreeNode;
+
+public class FlattenBinaryTreeToLinkedList {
+    TreeNode pre = null;
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = pre;
+        root.left = null;
+        pre = root;
+    }
+}
