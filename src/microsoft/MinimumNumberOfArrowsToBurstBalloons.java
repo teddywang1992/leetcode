2572b2c3700b61e3d580a393380e52680208d3ea
teddywang <443909723@qq.com>
2017-10-22 11:29:11 -0700

    microsoft

---
+++ src/microsoft/MinimumNumberOfArrowsToBurstBalloons.java
@@ -0,0 +1,28 @@
+package microsoft;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MinimumNumberOfArrowsToBurstBalloons {
+    public int findMinArrowShots(int[][] points) {
+        if (points == null || points.length == 0 || points[0].length == 0) return 0;
+        Arrays.sort(points, new Comparator<int[]>() {
+            public int compare(int[] a1, int[] a2) {
+                return a1[0] - a2[0];
+            }
+        });
+
+        int count = 1;
+        int end = points[0][1];
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] <= end) {
+                end = Math.min(end, points[i][1]);
+            } else {
+                count++;
+                end = points[i][1];
+            }
+        }
+
+        return count;
+    }
+}
