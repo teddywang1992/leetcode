c8649eb9561bd258ab5da1fd6ea3f4802a001850
teddywang <443909723@qq.com>
2017-10-21 20:31:46 -0700

    microsoft

---
+++ src/microsoft/PermutationinString.java
@@ -0,0 +1,40 @@
+package microsoft;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PermutationinString {
+    public static void main(String[] args) {
+        String s1 = "adc";
+        String s2 = "cdca";
+
+        System.out.println(checkInclusion(s1, s2));
+    }
+
+    public static boolean checkInclusion(String s1, String s2) {
+        int len1 = s1.length(), len2 = s2.length();
+        if (len1 > len2) return false;
+
+        int[] count = new int[26];
+        for (int i = 0; i < len1; i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        if (allZero(count)) return true;
+
+        for (int i = len1; i < len2; i++) {
+            count[s2.charAt(i) - 'a']--;
+            count[s2.charAt(i - len1) - 'a']++;
+            if (allZero(count)) return true;
+        }
+
+        return false;
+    }
+
+    private static boolean allZero(int[] count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
+    }
+}
