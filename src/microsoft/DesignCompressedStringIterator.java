8e111793b8f56d1f4b23b6ca38d2c7a468cd81ec
teddywang <443909723@qq.com>
2017-10-22 20:24:09 -0700

    microsoft

---
+++ src/microsoft/DesignCompressedStringIterator.java
@@ -0,0 +1,29 @@
+package microsoft;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DesignCompressedStringIterator {
+    Queue<int[]> queue = new LinkedList<>();
+
+    public DesignCompressedStringIterator(String s) {
+        int i = 0, n = s.length();
+        while (i < n) {
+            int j = i+1;
+            while (j < n && s.charAt(j) - 'A' < 0) j++;
+            queue.add(new int[]{s.charAt(i) - 'A',  Integer.parseInt(s.substring(i+1, j))});
+            i = j;
+        }
+    }
+
+    public char next() {
+        if (queue.isEmpty()) return ' ';
+        int[] top = queue.peek();
+        if (--top[1] == 0) queue.poll();
+        return (char) ('A' + top[0]);
+    }
+
+    public boolean hasNext() {
+        return !queue.isEmpty();
+    }
+}
