c86be5b37775e49755618cca52e6051f1245de4b
teddywang <443909723@qq.com>
2017-10-25 10:22:12 -0700

    microsoft

---
+++ src/microsoft/TwoSum.java
@@ -0,0 +1,18 @@
+package microsoft;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return new int[]{};
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) return new int[] {map.get(target - nums[i]), i};
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
