c50ed3071122c0ac681f9b4444ca01c2a45cde43
teddywang <443909723@qq.com>
2017-10-24 21:08:25 -0700

    microsoft

---
+++ src/microsoft/LowestCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,11 @@
+package microsoft;
+
+import tree.TreeNode;
+
+public class LowestCommonAncestorofaBinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        else if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        else return root;
+    }
+}
