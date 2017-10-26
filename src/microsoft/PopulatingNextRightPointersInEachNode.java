e6cc8620e7436e38944592e99392a9f32f1467ce
teddywang <443909723@qq.com>
2017-10-26 13:13:26 -0700

    microsoft

---
+++ src/microsoft/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,26 @@
+package microsoft;
+
+public class PopulatingNextRightPointersInEachNode {
+    public void connect(TreeLinkNode root) {
+        if (root == null) return;
+        TreeLinkNode cur = null;
+
+        while (root.left != null) {
+            cur = root;
+            while (cur != null) {
+                cur.left.next = cur.right;
+
+                if (cur.next != null) {
+                    cur.right.next = cur.next.left;
+                }
+                cur = cur.next;
+            }
+            root = root.left;
+        }
+    }
+    class TreeLinkNode {
+        int val;
+        TreeLinkNode left, right, next;
+        TreeLinkNode(int x) { val = x; }
+    }
+}
