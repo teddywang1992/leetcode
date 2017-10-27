f70d5cbed3e289be84fe6921c776b762cb34fac2
teddywang <443909723@qq.com>
2017-10-27 13:24:19 -0700

    microsoft

---
+++ src/microsoft/RomanToInteger.java
@@ -0,0 +1,30 @@
+package microsoft;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int result = map.get(s.charAt(0));
+
+        for (int i = 1; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) {
+                result += map.get(s.charAt(i)) - 2 * map.get(s.charAt(i - 1));
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+
+        return result;
+    }
+}
