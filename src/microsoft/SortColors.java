78d515c7985daf53639d608af9221ecc6cff2855
teddywang <443909723@qq.com>
2017-10-26 22:16:39 -0700

    microsoft

---
+++ src/microsoft/SortColors.java
@@ -0,0 +1,28 @@
+package microsoft;
+
+public class SortColors {
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int left = 0;
+        int right = nums.length - 1;
+        int i = 0;
+        while (i <= right) {
+            if (nums[i] == 0) {
+                swap(nums, i, left);
+                i++;
+                left++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, right);
+                right--;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
