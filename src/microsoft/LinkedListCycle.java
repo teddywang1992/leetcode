e0db4ab266b53dc0cc7b9106081867e6d8b59019
teddywang <443909723@qq.com>
2017-10-27 11:01:20 -0700

    microsoft

---
+++ src/microsoft/LinkedListCycle.java
@@ -0,0 +1,19 @@
+package microsoft;
+
+import linkedList.ListNode;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+}
