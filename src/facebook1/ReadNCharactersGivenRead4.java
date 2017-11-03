cf668d8d60b181ab752c3c7312bd2172e1d8d394
teddywang <443909723@qq.com>
2017-11-03 16:42:51 -0700

    facebook

---
+++ src/facebook1/ReadNCharactersGivenRead4.java
@@ -0,0 +1,17 @@
+package facebook1;
+
+public class ReadNCharactersGivenRead4 {
+//    public int read(char[] buf, int n) {
+//        int i = 0;
+//        while (i < n) {
+//            char[] buf2 = new char[4];
+//            int i2 = 0, n2 = read4(buf2);
+//            if (n2 == 0) break;
+//            while (i < n && i2 < n2) {
+//                buf[i++] = buf2[i2++];
+//            }
+//        }
+//
+//        return i;
+//    }
+}
