7f942893d064e856f2799c4dbc28a349be5c7ba9
teddywang <443909723@qq.com>
2017-11-03 16:51:50 -0700

    facebook

---
+++ src/facebook1/TwoSum.java
@@ -0,0 +1,17 @@
+package facebook1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return new int[]{};
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) return new int[] {i, map.get(target - nums[i])};
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
