5708ee0ff384db188e7688e906e815adfb371abb
teddywang <443909723@qq.com>
2017-11-03 13:55:36 -0700

    facebook

--- src/facebook1/MoveZeroes1.java
+++ src/facebook1/MoveZeroes1.java
@@ -1,5 +1,15 @@
 package facebook1;
 
 public class MoveZeroes1 {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) nums[left++] = nums[right];
+        }
 
+        while (left < nums.length) {
+            nums[left++] = 0;
+        }
+    }
 }
