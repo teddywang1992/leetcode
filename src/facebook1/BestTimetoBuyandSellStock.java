62623fcacc1c9953e84171e2a7443253876dd819
teddywang <443909723@qq.com>
2017-11-03 16:47:40 -0700

    facebook

--- src/facebook1/BestTimetoBuyandSellStock.java
+++ src/facebook1/BestTimetoBuyandSellStock.java
@@ -1,4 +1,15 @@
 package facebook1;
 
 public class BestTimetoBuyandSellStock {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for (int price : prices) {
+            min = Math.min(min, price);
+            max = Math.max(max, price - min);
+        }
+
+        return max;
+    }
 }
