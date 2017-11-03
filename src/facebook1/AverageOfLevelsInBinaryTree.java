5734eacc6c2ae631dd6810764d6c167ebf665e3b
teddywang <443909723@qq.com>
2017-11-03 15:24:35 -0700

    facebook

---
+++ src/facebook1/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,30 @@
+package facebook1;
+
+import tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class AverageOfLevelsInBinaryTree {
+    public List<Double> averageOfLevels(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        List<Double> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            double sum = 0;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                sum += cur.val;
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+            result.add(sum / size);
+        }
+
+        return result;
+    }
+}
