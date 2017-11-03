1cef1a49a54c15591f9df40fad6f9542528eb0bd
teddywang <443909723@qq.com>
2017-11-03 15:27:38 -0700

    facebook

---
+++ src/facebook1/FirstBadVersion.java
@@ -0,0 +1,18 @@
+package facebook1;
+
+public class FirstBadVersion {
+//    public int firstBadVersion(int n) {
+//        int start = 0;
+//        int end = n;
+//        while (start + 1 < end) {
+//            int mid = start + (end - start) / 2;
+//            if (isBadVersion(mid)) {
+//                end = mid;
+//            } else {
+//                start = mid;
+//            }
+//        }
+//
+//        return isBadVersion(start) ? start : end;
+//    }
+}
