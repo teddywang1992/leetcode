e509cde468a86c8317139446e713cbd2e21f2ce5
teddywang <443909723@qq.com>
2017-11-04 10:21:01 -0700

    facebook

--- src/facebook1/GraphValidTree.java
+++ src/facebook1/GraphValidTree.java
@@ -1,7 +1,33 @@
 package facebook1;
 
-public class GraphValidTree {
+import java.util.Arrays;
 
+public class GraphValidTree {
+    public boolean validTree(int n, int[][] edges) {
+        // initialize n isolated islands
+        int[] nums = new int[n];
+        Arrays.fill(nums, -1);
+
+        // perform union find
+        for (int i = 0; i < edges.length; i++) {
+            int x = find(nums, edges[i][0]);
+            int y = find(nums, edges[i][1]);
+
+            // if two vertices happen to be in the same set
+            // then there's a cycle
+            if (x == y) return false;
+
+            // union
+            nums[x] = y;
+        }
+
+        return edges.length == n - 1;
+    }
+
+    int find(int nums[], int i) {
+        if (nums[i] == -1) return i;
+        return find(nums, nums[i]);
+    }
 
     //     public boolean validTree(int n, int[][] edges) {
 //         Set<Integer>[] sets = new HashSet[n];
