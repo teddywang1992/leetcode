956b082c7da0ef7a124125fd50d60fa92ed5a3e4
teddywang <443909723@qq.com>
2017-11-04 14:28:30 -0700

    facebook

---
+++ src/facebook1/WordBreak.java
@@ -0,0 +1,26 @@
+package facebook1;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>();
+        for (String word : wordDict) {
+            set.add(word);
+        }
+
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
