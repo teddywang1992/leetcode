965dfa0ba9005d8389cb0b6ee4129db4fc3209cd
teddywang <443909723@qq.com>
2017-11-04 14:09:22 -0700

    facebook

---
+++ src/facebook1/FlattenNestedListIterator.java
@@ -0,0 +1,28 @@
+package facebook1;
+
+public class FlattenNestedListIterator {
+//    Stack<NestedInteger> stack;
+//    public NestedIterator(List<NestedInteger> nestedList) {
+//        stack = new Stack<>();
+//        for (int i = nestedList.size() - 1; i >= 0; i--) {
+//            stack.push(nestedList.get(i));
+//        }
+//    }
+//
+//    @Override
+//    public Integer next() {
+//        return stack.pop().getInteger();
+//    }
+//
+//    @Override
+//    public boolean hasNext() {
+//        while (!stack.isEmpty() && !stack.peek().isInteger()) {
+//            List<NestedInteger> list = stack.pop().getList();
+//            for (int i = list.size() - 1; i >= 0; i--) {
+//                stack.push(list.get(i));
+//            }
+//        }
+//
+//        return !stack.isEmpty();
+//    }
+}
