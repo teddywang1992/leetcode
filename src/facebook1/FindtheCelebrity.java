aaafe66fb28fac677df4bf816c1b72a9fd3c480e
teddywang <443909723@qq.com>
2017-11-04 18:42:37 -0700

    facebook

---
+++ src/facebook1/FindtheCelebrity.java
@@ -0,0 +1,16 @@
+package facebook1;
+
+public class FindtheCelebrity {
+//    public int findCelebrity(int n) {
+//        int result = 0;
+//        for (int i = 0; i < n; i++) {
+//            if (knows(result, i)) result = i;
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            if (i != result && (knows(result, i) || !(knows(i, result)))) return -1;
+//        }
+//
+//        return result;
+//    }
+}
