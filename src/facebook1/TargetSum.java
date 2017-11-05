f3a32271ec65c90f32a70e3d5e1a52cdf9504a7d
teddywang <443909723@qq.com>
2017-11-05 09:25:02 -0800

    facebook

---
+++ src/facebook1/TargetSum.java
@@ -0,0 +1,21 @@
+package facebook1;
+
+public class TargetSum {
+    public int hIndex(int[] citations) {
+        int[] temp = new int[citations.length + 1];
+
+        for (int citation : citations) {
+            int index = Math.min(citations.length, citation);
+            temp[index]++;
+        }
+        int h = 0;
+        for (int i = citations.length; i >= 0; i--) {
+            h += temp[i];
+            if (h >= i) {
+                return i;
+            }
+        }
+
+        return h;
+    }
+}
