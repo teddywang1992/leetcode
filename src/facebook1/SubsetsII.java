da7ca14a7edc5ea57e1e4560816f598ab0ba37e3
teddywang <443909723@qq.com>
2017-11-05 21:44:24 -0800

    facebook

---
+++ src/facebook1/SubsetsII.java
@@ -0,0 +1,26 @@
+package facebook1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) return result;
+        Arrays.sort(nums);
+        helper(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+
+    private void helper(List<List<Integer>> result, List<Integer> list, int[] nums, int pos) {
+        result.add(new ArrayList<>(list));
+
+        for (int i = pos; i < nums.length; i++) {
+            if (i != pos && nums[i] == nums[i - 1]) continue;
+            list.add(nums[i]);
+            helper(result, list, nums, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
