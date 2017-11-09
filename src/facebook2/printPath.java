1f7787b53f1cd661076a35ff7698f41f319bfa58
teddywang <443909723@qq.com>
2017-11-08 18:58:21 -0800

    facebook

--- src/facebook2/printPath.java
+++ src/facebook2/printPath.java
@@ -21,11 +21,6 @@ public class printPath {
 //    ACE
 //            ACF
 //    http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=171687&extra=page%3D1%26filter%3Dsortid%26sortid%3D311%26searchoption%5B3090%5D%5Bvalue%5D%3D1%26searchoption%5B3090%5D%5Btype%5D%3Dradio%26searchoption%5B3046%5D%5Bvalue%5D%3D2%26searchoption%5B3046%5D%5Btype%5D%3Dradio%26sortid%3D311
-    // do the transic every day
-// when today's prices is higher than yesterday
-// if pricse keep increase, the result will be the same as buy at first day and
-// sell at last day
-
 
 //    print path to leaf (BT path leaf)
 //
