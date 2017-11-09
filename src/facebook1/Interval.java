9caeaca270289e430c52ba7df7eccfb3bcf9b552
teddywang <443909723@qq.com>
2017-11-08 20:14:24 -0800

    facebook

--- src/facebook1/Interval.java
+++ src/facebook1/Interval.java
@@ -1,9 +1,9 @@
 package facebook1;
 
 public class Interval {
-    int start;
-    int end;
-    Interval() { start = 0; end = 0; }
-    Interval(int s, int e) { start = s; end = e; }
+    public int start;
+    public int end;
+    public Interval() { start = 0; end = 0; }
+    public Interval(int s, int e) { start = s; end = e; }
 
 }
