241d9d693b3e1c91695216b465de51a5587f5de7
teddywang <443909723@qq.com>
2017-11-09 16:39:23 -0800

    facebook

---
+++ src/facebook1/ReadNCharactersGivenRead4IICallmultipletime.java
@@ -0,0 +1,27 @@
+package facebook1;
+
+public class ReadNCharactersGivenRead4IICallmultipletime {
+    private int i2 = 0;
+    private int n2 = 0;
+    private char[] buf2 = new char[4];
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        int i = 0;
+        while (i < n) {
+            if (i2 == n2) {
+                i2 = 0;
+//                n2 = read4(buf2);
+            }
+            if (n2 == 0) break;
+            while (i < n && i2 < n2) {
+                buf[i++] = buf2[i2++];
+            }
+        }
+
+        return i;
+    }
+}
