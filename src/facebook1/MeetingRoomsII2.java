394a412921d76eb1a96aca5eb66f2cbaf1f42b3e
teddywang <443909723@qq.com>
2017-11-11 14:57:38 -0800

    facebook

---
+++ src/facebook1/MeetingRoomsII2.java
@@ -0,0 +1,22 @@
+package facebook1;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class MeetingRoomsII2 {
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) return 0;
+        Arrays.sort(intervals, (i1, i2) -> (i1.start - i2.start));
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        heap.offer(intervals[0].end);
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start >= heap.peek()) {
+                heap.poll();
+            }
+            heap.offer(intervals[i].end);
+        }
+
+        return heap.size();
+    }
+}
