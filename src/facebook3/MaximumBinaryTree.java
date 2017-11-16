1d07b8793001f10cfddb005e257f22cc2a132da5
teddywang <443909723@qq.com>
2017-11-15 20:05:48 -0800

    facebook

---
+++ src/facebook3/MaximumBinaryTree.java
@@ -0,0 +1,24 @@
+package facebook3;
+
+import tree.TreeNode;
+
+public class MaximumBinaryTree {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if (nums == null || nums.length == 0) return null;
+        return helper(nums, 0, nums.length- 1);
+    }
+
+    private TreeNode helper(int[] nums, int start, int end) {
+        if (start > end) return null;
+        int maxIndex = start;
+        for (int i = start; i <= end; i++) {
+            if (nums[maxIndex] < nums[i]) {
+                maxIndex = i;
+            }
+        }
+        TreeNode root = new TreeNode(nums[maxIndex]);
+        root.left = helper(nums, start, maxIndex - 1);
+        root.right = helper(nums, maxIndex + 1, end);
+        return root;
+    }
+}
