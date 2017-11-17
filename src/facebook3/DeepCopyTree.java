8f782467a70e379751de9bec78ef0f982d354e53
teddywang <443909723@qq.com>
2017-11-17 11:19:39 -0800

    facebook

---
+++ src/facebook3/DeepCopyTree.java
@@ -0,0 +1,23 @@
+package facebook3;
+
+import tree.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DeepCopyTree {
+    Map<TreeNode, TreeNode> map = new HashMap<>();
+    public TreeNode cloneTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode newRoot;
+        if (map.containsKey(root)) {
+            newRoot = map.get(root);
+        }
+        else {
+            newRoot = new TreeNode(root.val);
+        }
+        newRoot.left = cloneTree(root.left);
+        newRoot.right = cloneTree(root.right);
+        return newRoot;
+    }
+}
