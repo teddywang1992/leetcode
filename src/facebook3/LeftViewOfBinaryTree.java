e2b2901dd629cefaa1e4cb339284bacc9b41f79d
teddywang <443909723@qq.com>
2017-11-16 20:19:11 -0800

    facebook

--- src/facebook3/LeftViewOfBinaryTree.java
+++ src/facebook3/LeftViewOfBinaryTree.java
@@ -1,4 +1,52 @@
 package facebook3;
 
+import tree.TreeNode;
+import tree.constantTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class LeftViewOfBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = constantTree.returnRoot();
+        System.out.println(leftSideView(root));
+    }
+
+    private static List<Integer> leftSideView(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (i == 0) result.add(cur.val);
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+        }
+        return result;
+    }
+
+    public static List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (i == size - 1) result.add(cur.val);
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+        }
+
+        return result;
+    }
 }
