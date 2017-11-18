cc33c46197768fc22b195c5d832bf74746bac54d
teddywang <443909723@qq.com>
2017-11-18 15:03:12 -0800

    facebook

--- src/facebook3/MoveZeroes.java
+++ src/facebook3/MoveZeroes.java
@@ -1,7 +1,33 @@
 package facebook3;
 
 public class MoveZeroes {
-    public void moveZeroes(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,0,2,13};
+        moveZero(nums);
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+
+    private static void moveZero(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            while (start < nums.length && nums[start] != 0) start++;
+            while (end >= 0 && nums[end] == 0) end--;
+            if (start < end) {
+                swap(nums, start, end);
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void moveZeroes(int[] nums) {
         if (nums == null || nums.length == 0) return;
         int left = 0;
         for (int right = 0; right < nums.length; right++) {
