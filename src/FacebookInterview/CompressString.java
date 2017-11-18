a1ae7dcdc72022a16d311f839e5fb63dd1f1dab3
teddywang <443909723@qq.com>
2017-11-18 15:01:09 -0800

    facebook

---
+++ src/FacebookInterview/CompressString.java
@@ -0,0 +1,23 @@
+package FacebookInterview;
+
+public class CompressString {
+    public static void main(String[] args) {
+        String s = "a";
+        System.out.println(compress(s));
+    }
+
+    private static String compress(String s) {
+        if (s == null) return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            int count = 1;
+            char c = s.charAt(i);
+            while (i < s.length() - 1 && s.charAt(i) == s.charAt(i+1)) {
+                count++;
+                i++;
+            }
+            sb.append(count).append(c);
+        }
+        return sb.toString();
+    }
+}
